fb885f42c953c6cfcaa310beaf36967317c2ee08
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-07 09:23:22 +0300

    Code along

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,6 +4,25 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      // call the onAddItem prop with the newItem
+      .then((newItem) => onAddItem(newItem));
+  }
+
   return (
     <form className="NewItem">
       <label>
